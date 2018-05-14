56775ba2ff8c019a1624437736ff50268c7bf455
patelabhi191 <36008861+patelabhi191@users.noreply.github.com>
2018-05-14 18:17:24 -0400

    [Add] account and test class with constructor

---
+++ src/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+    private String name;
+
+    public Account(String name){
+        this.name=name;
+    }
+    //To set account name
+    public void setName(String name) {
+        this.name=name;
+    }
+    //To get account name
+    public String getName() {
+        return name;
+    }
+}
