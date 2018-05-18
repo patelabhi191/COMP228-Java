2df81daf8ddab2b2720439a4adf9da614b18a758
patelabhi191 <36008861+patelabhi191@users.noreply.github.com>
2018-05-18 09:39:45 -0400

    [Add] Assign-1Patient

---
+++ src/Patient.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Patient {
+    int patient_id;
+    Character firstname;
+    Character lastname;
+    String address;
+    String city;
+    String province;
+    int postalcode;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+
+}
